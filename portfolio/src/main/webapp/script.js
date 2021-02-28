c4137c3db3694fd16a4f25021edb848adb367721
Mufaro Makiwa <mmakiwa@mit.edu>
2021-02-28 04:02:58 +0000

    Adds navbar to the portfolio page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,32 +18,19 @@
 function addRandomGreeting() {
 
   const quotes = [
-    "Red: If people were more kind and loving, then the world would be too.",
-
+    "Red: If people were more kind and loving, then the world would be too.",  
     "Aram: I’m not saying I’m amazing, but sometimes I’m distinctly above average.",
-
     "Red: As a fugitive of justice, I prefer not to linger in hallways.",
-
     "Red: Wisdom is learning the boundaries of one’s designated lane.",
-
     "Red: Read the book, think positive, control your urges, and I’ll put your gift to good use.",
-
     "Red: You’re a good friend who takes more care of me than I deserve.",
-
     "Red:  Revenge isn’t a passion. It’s a disease. It eats at your mind and poisons your soul.",
-
     "Scottie: There’s a very fine line between love and hate.",
-
     "Red: Sins should be buried like the dead. Not that they may be forgotten but we may them and find our way forward nonetheless.",
-
     "Red: No matter what I tried to do, all I brought her was misery and violence.",
-
     "Red: As my grandmother said, it’s good to have friends in low places.",
-
     "Red: Leave the past in the past, Tom. Nothing good comes from digging up secrets.",
-
     "Red: Sometimes bad luck is the best luck you’ll ever have.",
-
     "Sinclair: Whoever you are, confronting it with violence, is no path to resolution.",
   ]
   
