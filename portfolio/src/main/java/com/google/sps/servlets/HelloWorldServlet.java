375cec1980ca6c60a82ee786626c86510ff22d07
Mufaro Makiwa <mmakiwa@mit.edu>
2021-03-04 06:44:15 +0000

    Adds messages to helloworldservlet.java

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,22 +1,36 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-  private int getReqests = 0;
+  List<String> quotes = new ArrayList(List.of(
+    "Red: If people were more kind and loving, then the world would be too.",  
+    "Aram: I’m not saying I’m amazing, but sometimes I’m distinctly above average.",
+    "Scottie: There’s a very fine line between love and hate."
+  ));
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    getReqests++;
-
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello world: " + "x" + getReqests);
+    response.setContentType("application/json");
+    response.getWriter().println(convertToJson(quotes));
   }
+
+
+  private String convertToJson(List<String> quotes) {
+    Gson gson = new Gson();
+    String arrayToJson = gson.toJson(quotes);
+    return arrayToJson;
+  } 
 }
--- portfolio/src/main/webapp/server.js
+++ portfolio/src/main/webapp/server.js
@@ -1,7 +1,6 @@
 const getMessage = async () => {
   const responseFromServer = await fetch('/hello');
   const textFromResponse = await responseFromServer.text();
-  console.log(textFromResponse);
   const helloContainer = document.getElementById('server-text-container');
   helloContainer.innerText = textFromResponse;
 }
