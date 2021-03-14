69f73d844ba4b029386c94907957e9daba3ad818
Mufaro Makiwa <mmakiwa@mit.edu>
2021-03-13 23:40:00 -0500

    Connects contact page to servlet

---
+++ portfolio/src/main/webapp/contact.js
@@ -0,0 +1,47 @@
+let userName = false, userEmail = false, subject = false, message = false;
+let buttonDisabled = true;
+
+const updateField = (element, field) => {
+  let text = element.value;
+  console.log(text)
+}
+
+const onNameChange = (element) => {
+  value = element.value;
+  value !== "" ? userName = true : userName = false;
+  updateField(element, userName);
+  enableSubmitButton();
+}
+
+const onEmailChange = (element) => {
+  value = element.value;
+  value !== "" ? userEmail = true : userEmail = false;
+  enableSubmitButton();
+}
+
+const onSubjectChange = (element) => {
+  value = element.value;
+  value !== "" ? subject = true : subject = false;
+  enableSubmitButton();
+}
+
+const onMessageChange = (element) => {
+  value = element.value;
+  value !== "" ? message = true : message = false;
+  enableSubmitButton();
+}
+
+const enableSubmitButton = () => {
+  if (userName && userEmail && subject && message) {
+    if (buttonDisabled) {
+      // enable button
+      document.getElementById("submit_button").disabled = false;
+    }
+  } else {
+    if (!buttonDisabled) {
+      // disable button
+      document.getElementById("submit_button").disabled = true;
+    }
+  }
+}
+
